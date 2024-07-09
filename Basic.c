5449c7d23c181bd6a8ef34afc0aa2d9bd256a5fb
BoghdadyAhmed <132693797+BoghdadyAhmed@users.noreply.github.com>
2024-07-09 13:20:47 +0000

    one

---
+++ Basic.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+//C buliding process 
+//file.c //tool chain // preprocessor //compiler //assembler //linker 
+//build run 
+//preprocessor remove # + commint 
+//compiler C code to assembly code 
+//assembler assembly code to machine code 
+//linker files assembly give .exe or .hex
+printf("\"Helo\"\n");
+printf("\'Hello\'\n");
+// \n new line 
+// \t 4tap
+int num;
+int x;
+float y;
+//num =60;
+scanf("%d\n%d\n%f",&num ,&x ,&y);
+printf("%d\n%d\n%f\n",num,x,y);
+//---------------------------------------------------------
+//casting in c
+
+
+
+
+
+
+
+
+
+
+
+
+}
